0d158cdf3cd026c12174bb07888e8bfeac053154
Jeremy Lincoln <bossco28@gmail.com>
2017-12-13 08:03:13 -0500

    Add routes add, edit, delete

---
+++ server.js
@@ -0,0 +1,93 @@
+const express = require('express');
+const hbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+
+//app
+const app = express();
+
+////########## HBS SETUP ##########////
+//app engine
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultLayout: 'layout',
+    layoutsDir: __dirname+ '/views/layouts',
+    partialsDir: __dirname + '/views/partials'
+}));
+
+//setup view engine
+app.set('view engine', 'hbs');
+
+//setup css
+app.use('/css', express.static(__dirname+'/public/css'));
+
+//request - middleware
+const jsonParser = bodyParser.json();
+
+
+////########## ROUTES ##########////
+
+//GET
+app.get('/', (req,res)=>{
+    fetch('http://localhost:3004/messages')
+        .then(response => {
+            response.json().then(json => {
+                res.render('home', {
+                    articles: json
+                })
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
+});
+
+app.get('/add_note', (req, res)=>{
+    res.render('add_note');
+});
+
+//EDIT
+app.get('/edit_note/:id', (req, res)=>{
+    console.log('hit');
+    fetch(`http://localhost:3004/messages/${req.params.id}`)
+        .then(response => {
+            response.json().then(json => {
+                res.render('edit_note', {
+                    articles:json
+                })
+            })
+        })
+});
+
+
+//POST
+app.post('/api/add_note', jsonParser, (req, res)=>{
+    fetch('http://localhost:3004/messages',{
+        method:'POST',
+        body: JSON.stringify(req.body),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then((response)=>{
+        res.status(200).send()
+    });
+});
+
+//DELETE
+app.delete('/api/delete/:id', (req, res)=> {
+    const id = req.params.id;
+    fetch(`http://localhost:3004/messages/${id}`, {
+        method:'DELETE'
+    }).then(response => {
+        res.status(200).send();
+    })
+});
+
+
+
+//Server
+const port = process.env.PORT || 3000;
+app.listen(port, ()=>{
+    console.log(`Server running on port: ${port}`);
+});
